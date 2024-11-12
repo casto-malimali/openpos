7b975f381277c25b529aaff8e33916e636dba23d
Casto MALIMALI <castomalimali@gmail.com>
2024-11-12 15:53:57 +0300

    ,...

---
+++ application/views/partial/footer.php
@@ -0,0 +1,12 @@
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="jumbotron push-spaces">
+			<strong><?php echo $this->lang->line('common_copyrights', date('Y')); ?> · 
+			<a href="victorstephen.co.tz " target="_blank"><?php echo 'VictorPOS'; ?></a>  · 
+			 - <a target="_blank" href="victorstephen.co.tz ></a></strong>.
+		</div>
+	</div>
+</body>
+</html>
